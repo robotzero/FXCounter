18dc2fb4c0816635e69d0a8273720d124dff563a
Agnieszka K. Kocinski <0robotzero@gmail.com>
2016-04-17 19:21:31 +0100

    Switching to events.

--- src/main/java/com/queen/counter/service/Scroller.java
+++ src/main/java/com/queen/counter/service/Scroller.java
@@ -10,9 +10,11 @@ import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.Node;
 import javafx.scene.text.Text;
+import org.reactfx.*;
 
 import java.time.LocalTime;
 import java.util.List;
+
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -20,96 +22,79 @@ public class Scroller {
 
     private final Animator animator;
     private final InMemoryCachedServiceLocator cache;
-    private BooleanProperty delta = new SimpleBooleanProperty(true);
-    private BooleanProperty label = new SimpleBooleanProperty(true);
+    private BooleanProperty delta = new SimpleBooleanProperty(false);
+    private BooleanProperty label = new SimpleBooleanProperty(false);
 
     private boolean first = true;
+    private IntegerProperty offset = new SimpleIntegerProperty(1);
     private IntegerProperty seconds = new SimpleIntegerProperty();
     private IntegerProperty minutes = new SimpleIntegerProperty();
-    private LocalTime mainClock = LocalTime.of(0, 12, 12);
+
+    private final EventSource<Boolean> foundNode = new EventSource<>();
 
     public Scroller(final Animator animator, final InMemoryCachedServiceLocator cache) {
         this.animator = animator;
         this.cache = cache;
 
-        delta.addListener((observable, oldValue, newValue) -> {
-            first = true;
-        });
 
-        label.addListener((observable, oldValue, newValue) -> {
-            first = true;
-        });
+        EventStream lab = EventStreams.changesOf(label);
+        EventStream del = EventStreams.changesOf(delta);
+
+        EventStreams.combine(lab, foundNode).map(change -> {
+            return 3;
+        }).feedTo(offset);
+        EventStreams.changesOf(delta).or(lab).map(change -> {
+            return 2;
+        }).feedTo(offset);
+         EventStreams.combine(del, foundNode).map(change -> {
+            return 3;
+        }).feedTo(offset);
     }
 
     public void scroll(final List<Node> rectangles, final List<Text> labels, double deltaY) {
-        final int compare;
-        final boolean label = rectangles.get(0).getId().contains("seconds");
-        this.label.set(label);
-        if (label) {
+
+        final int found = deltaY > 0 ? 0 : 240;
+        final int compare = deltaY < 0 ? 0 : 240;
+        delta.set(deltaY < 0);
+
+        this.label.set(rectangles.get(0).getId().contains("seconds"));
+        rectangles.stream().filter(r -> r.getTranslateY() == found).findAny().ifPresent(r -> foundNode.push(true));
+
+        int offsetNumber = offset.getValue();
+
+        if (label.get()) {
             animator.setRunning(true);
         } else {
             animator.setMinutesRunning(true);
         }
-        delta.set(deltaY < 0);
+
         if (delta.get()) {
-            mainClock = clockTick(mainClock, clock -> clock.minusSeconds(1));
-            if (label) {
-                ClockPresenter.userTimeSeconds = ClockPresenter.userTimeSeconds.minusSeconds(1);
+            if (label.get()) {
+                ClockPresenter.userTimeSeconds = ClockPresenter.userTimeSeconds.minusSeconds(offsetNumber);
             } else {
-                ClockPresenter.userTimeMinutes = ClockPresenter.userTimeMinutes.minusMinutes(1);
+                ClockPresenter.userTimeMinutes = ClockPresenter.userTimeMinutes.minusMinutes(offsetNumber);
             }
-            if (first) {
-                if (label) {
-                    ClockPresenter.time = ClockPresenter.userTimeSeconds.minusSeconds(1);
-                } else {
-                    ClockPresenter.minutesTime = ClockPresenter.userTimeMinutes.minusMinutes(1);
-                }
-            } else {
-                if (label) {
-                    ClockPresenter.time = ClockPresenter.time.minusSeconds(1);
-                } else {
-                    ClockPresenter.minutesTime = ClockPresenter.minutesTime.minusMinutes(1);
-                }
-            }
-            compare = 0;
         } else {
-            mainClock = clockTick(mainClock, clock -> clock.plusSeconds(1));
-            if (label) {
-                ClockPresenter.userTimeSeconds = ClockPresenter.userTimeSeconds.plusSeconds(1);
+            if (label.get()) {
+                ClockPresenter.userTimeSeconds = ClockPresenter.userTimeSeconds.plusSeconds(offsetNumber);
             } else {
-                ClockPresenter.userTimeMinutes = ClockPresenter.userTimeMinutes.plusMinutes(1);
+                ClockPresenter.userTimeMinutes = ClockPresenter.userTimeMinutes.plusMinutes(offsetNumber);
             }
-            if (first) {
-                if (label) {
-                    ClockPresenter.time = ClockPresenter.userTimeSeconds.plusSeconds(1);
-                } else {
-                    ClockPresenter.minutesTime = ClockPresenter.userTimeMinutes.plusMinutes(1);
-                }
-            } else {
-                if (label) {
-                    ClockPresenter.time = ClockPresenter.time.plusSeconds(1);
-                } else {
-                    ClockPresenter.minutesTime = ClockPresenter.minutesTime.plusMinutes(1);
-                }
-            }
-            compare = 240;
         }
-        if (label) {
-            seconds.set(ClockPresenter.time.getSecond());
+        if (label.get()) {
+            seconds.set(ClockPresenter.userTimeSeconds.getSecond());
         } else {
-            minutes.set(ClockPresenter.minutesTime.getMinute());
+            minutes.set(ClockPresenter.userTimeMinutes.getMinute());
         }
-        System.out.println(mainClock);
+
         ClockPresenter.userTime = ClockPresenter.userTime.withSecond(ClockPresenter.userTimeSeconds.getSecond()).withMinute(ClockPresenter.userTimeMinutes.getMinute());
 
         List<AnimationMetadata> l = rectangles.stream().map(n -> (AnimationMetadata) cache.get(AnimationMetadata.class, n)).collect(Collectors.toList());
 
         rectangles.stream().filter(r -> r.getTranslateY() == compare).findAny().ifPresent(r -> {
             labels.stream().filter(lbl -> lbl.getId().equals(r.getId())).findFirst().ifPresent(lbl -> {
-                if (label) {
+                if (label.get()) {
                     lbl.setText(seconds.get() + "");
-                    System.out.println(seconds.get());
-                    System.out.println(mainClock.minusSeconds(2));
                 } else {
                     lbl.setText(minutes.get() + "");
                 }
@@ -117,7 +102,7 @@ public class Scroller {
         });
 
         animator.animate(l, deltaY, cache);
-        first = false;
+        offset.set(1);
     }
 
     private LocalTime clockTick(LocalTime current, Function<LocalTime, LocalTime> tick) {
