f96d17ed81f5ed06ce64cb59d376bc02dc864732
Agnieszka K. Kocinski <0robotzero@gmail.com>
2016-08-13 11:27:22 +0100

    All basic scroll tests pass.

--- src/test/java/com/queen/acceptance/ScrollTest.java
+++ src/test/java/com/queen/acceptance/ScrollTest.java
@@ -34,55 +34,107 @@ public class ScrollTest extends CounterAppIT {
     public static Object[][] scrolls() {
         return new Object[][]{
                 //@formatter:off
+                // Seconds UP
                 {sequence(
                         expected(60, "14", 240, "11", 0, "18", 180, "14"),
                         step(ColumnType.SECONDS, VerticalDirection.UP, 1)
                 )},
                 {sequence(
-                        expected(60, "14", 240, "11", 0, "18", 180, "14"),
-                        step(ColumnType.SECONDS, VerticalDirection.UP, 1)
-                )}
+                        expected(120, "14", 60, "15", 0, "18", 180, "14"),
+                        step(ColumnType.SECONDS, VerticalDirection.UP, 2)
+                )},
+                {sequence(
+                        expected(180, "14", 120, "15", 0, "18", 180, "14"),
+                        step(ColumnType.SECONDS, VerticalDirection.UP, 3)
+                )},
+                {sequence(
+                        expected(240, "14", 180, "15", 0, "18", 180, "14"),
+                        step(ColumnType.SECONDS, VerticalDirection.UP, 4)
+                )},
+                {sequence(
+                        expected(60, "18", 240, "15", 0, "18", 180, "14"),
+                        step(ColumnType.SECONDS, VerticalDirection.UP, 5)
+                )},
+                {sequence(
+                        expected(120, "18", 60, "19", 0, "18", 180, "14"),
+                        step(ColumnType.SECONDS, VerticalDirection.UP, 6)
+                )},
+                // Seconds DOWN
+                {sequence(
+                        expected(180, "10", 120, "11", 0, "18", 180, "14"),
+                        step(ColumnType.SECONDS, VerticalDirection.DOWN, 1)
+                )},
+                {sequence(
+                        expected(120, "10", 60, "11", 0, "18", 180, "14"),
+                        step(ColumnType.SECONDS, VerticalDirection.DOWN, 2)
+                )},
+                {sequence(
+                        expected(60, "10", 0, "11", 0, "18", 180, "14"),
+                        step(ColumnType.SECONDS, VerticalDirection.DOWN, 3)
+                )},
+                {sequence(
+                        expected(0, "10", 180, "7", 0, "18", 180, "14"),
+                        step(ColumnType.SECONDS, VerticalDirection.DOWN, 4)
+                )},
+                {sequence(
+                        expected(180, "6", 120, "7", 0, "18", 180, "14"),
+                        step(ColumnType.SECONDS, VerticalDirection.DOWN, 5)
+                )},
+                {sequence(
+                        expected(120, "6", 60, "7", 0, "18", 180, "14"),
+                        step(ColumnType.SECONDS, VerticalDirection.DOWN, 6)
+                )},
+                // Minutes UP
+                {sequence(
+                        expected(0, "14", 180, "11", 60, "18", 240, "15"),
+                        step(ColumnType.MINUTES, VerticalDirection.UP, 1)
+                )},
+                {sequence(
+                        expected(0, "14", 180, "11", 120, "18", 60, "19"),
+                        step(ColumnType.MINUTES, VerticalDirection.UP, 2)
+                )},
+                {sequence(
+                        expected(0, "14", 180, "11", 180, "18", 120, "19"),
+                        step(ColumnType.MINUTES, VerticalDirection.UP, 3)
+                )},
+                {sequence(
+                        expected(0, "14", 180, "11", 240, "18", 180, "19"),
+                        step(ColumnType.MINUTES, VerticalDirection.UP, 4)
+                )},
+                {sequence(
+                        expected(0, "14", 180, "11", 60, "22", 240, "19"),
+                        step(ColumnType.MINUTES, VerticalDirection.UP, 5)
+                )},
+                {sequence(
+                        expected(0, "14", 180, "11", 120, "22", 60, "23"),
+                        step(ColumnType.MINUTES, VerticalDirection.UP, 6)
+                )},
+                // Minutes DOWN
+                {sequence(
+                        expected(0, "14", 180, "11", 180, "14", 120, "15"),
+                        step(ColumnType.MINUTES, VerticalDirection.DOWN, 1)
+                )},
+                {sequence(
+                        expected(0, "14", 180, "11", 120, "14", 60, "15"),
+                        step(ColumnType.MINUTES, VerticalDirection.DOWN, 2)
+                )},
+                {sequence(
+                        expected(0, "14", 180, "11", 60, "14", 0, "15"),
+                        step(ColumnType.MINUTES, VerticalDirection.DOWN, 3)
+                )},
+                {sequence(
+                        expected(0, "14", 180, "11", 0, "14", 180, "11"),
+                        step(ColumnType.MINUTES, VerticalDirection.DOWN, 4)
+                )},
+                {sequence(
+                        expected(0, "14", 180, "11", 180, "10", 120, "11"),
+                        step(ColumnType.MINUTES, VerticalDirection.DOWN, 5)
+                )},
+                {sequence(
+                        expected(0, "14", 180, "11", 120, "10", 60, "11"),
+                        step(ColumnType.MINUTES, VerticalDirection.DOWN, 6)
+                )},
                 //@formatter:on
-        };
-    }
-
-    // DOWN
-//                { 1, VerticalDirection.DOWN, "#seconds", 180, 0, "10", "13"},
-//                { 2, VerticalDirection.DOWN, "#seconds", 120, 180, "10" , "9"},
-//                { 3, VerticalDirection.DOWN, "#seconds", 60, 120, "10", "9" },
-//                { 4, VerticalDirection.DOWN, "#seconds", 0, 60, "10", "9" },
-//                { 5, VerticalDirection.DOWN, "#seconds", 180, 0, "6", "9" },
-//                { 6, VerticalDirection.DOWN, "#seconds", 120, 180, "6", "5" },
-//                { 7, VerticalDirection.DOWN, "#seconds", 60, 120, "6", "5" },
-//                { 8, VerticalDirection.DOWN, "#seconds", 0, 60, "6", "5" },
-//                { 9, VerticalDirection.DOWN, "#seconds", 180, 0, "2", "5" },
-//                { 10, VerticalDirection.DOWN, "#seconds", 120, 180, "2", "1" },
-//                // UP
-
-//                { 2, VerticalDirection.UP, "#seconds", 120, 60, "14", "15" },
-//                { 3, VerticalDirection.UP, "#seconds", 180, 120, "14", "15" },
-//                { 4, VerticalDirection.UP, "#seconds", 240, 180, "14", "15" },
-//                { 5, VerticalDirection.UP, "#seconds", 60, 240, "18", "15" },
-//                { 6, VerticalDirection.UP, "#seconds", 120, 60, "18", "19" },
-//                { 7, VerticalDirection.UP, "#seconds", 180, 120, "18", "19" },
-//                { 8, VerticalDirection.UP, "#seconds", 240, 180, "18", "19" },
-//                { 9, VerticalDirection.UP, "#seconds", 60, 240, "22", "19" },
-
-    @DataProvider
-    public static Object[][] basicSecondsMinutesScrollsSetupUpDown() {
-
-
-        // Number of scrolls, direction, group, timewait, expected new translate, expected label
-        return new Object[][] {
-                { 1, VerticalDirection.UP, "#seconds", 650, 60, "14" },
-                { 2, VerticalDirection.UP, "#seconds", 650, 120, "14" },
-                { 3, VerticalDirection.UP, "#seconds", 650, 180, "14" },
-                { 4, VerticalDirection.UP, "#minutes", 650, 240, "18" },
-                { 5, VerticalDirection.UP, "#seconds", 650, 60, "18" },
-                { 6, VerticalDirection.UP, "#minutes", 650, 120, "22" },
-                { 7, VerticalDirection.UP, "#seconds", 650, 180, "18" },
-                { 8, VerticalDirection.UP, "#seconds", 650, 240, "18" },
-                { 9, VerticalDirection.UP, "#seconds", 650, 60, "22" },
         };
     }
 
@@ -102,7 +154,7 @@ public class ScrollTest extends CounterAppIT {
         sequence.steps.forEach(step -> {
             moveTo("#" + step.columnType.name().toLowerCase());
             IntStream.range(0, step.scrollsNumber).forEach(i -> {
-                scroll(step.scrollsNumber, step.direction);
+                scroll(step.direction);
                 try {
                     WaitFor.waitUntil(timeout(millis(TIME_WAIT)));
                 } catch (InterruptedException e) {
@@ -137,7 +189,7 @@ public class ScrollTest extends CounterAppIT {
             }
 
             if (exBottomRectangle.isPresent()) {
-                String lb =  minutes.getChildren().stream().filter(rk -> rk.getClass().equals(Text.class)).filter(tr -> tr.getId().equals(bottomIdSeconds)).map(tt -> ((Text) tt).getText()).findAny().get();
+                String lb =  minutes.getChildren().stream().filter(rk -> rk.getClass().equals(Text.class)).filter(tr -> tr.getId().equals(bottomIdMinutes)).map(tt -> ((Text) tt).getText()).findAny().get();
                 return exBottomRectangle.get().getTranslateY() == sequence.expectedValues.bottomPositionMinutes && lb.equals(sequence.expectedValues.bottomLabelMinutes);
             }
 
@@ -145,7 +197,6 @@ public class ScrollTest extends CounterAppIT {
         });
     }
 
-    // @TODO check minutes group as well.
     // @TODO check cases around 0 and 50
     // @TODO test ticking, including minutes are up when seconds are at 0
     // @TODO test that you can't move columns when time is ticking.
