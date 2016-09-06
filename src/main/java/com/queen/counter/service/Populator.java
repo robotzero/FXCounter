045db111ebf01dfd8b14d5190f98811bd8005f66
Agnieszka K. Kocinski <0robotzero@gmail.com>
2016-09-06 21:18:33 +0100

    Added arcs.

--- src/main/java/com/queen/counter/service/Populator.java
+++ src/main/java/com/queen/counter/service/Populator.java
@@ -9,11 +9,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Point2D;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.Stop;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.StrokeType;
 import javafx.scene.text.Text;
 import org.reactfx.EventSource;
 import org.reactfx.util.Tuple2;
@@ -38,9 +34,15 @@ public class Populator {
     public Column create(StackPane stack) {
         stack.getChildren().stream().filter(child -> child.getClass().equals(Rectangle.class)).map(reg -> reg).forEach(r -> {
             Rectangle rect = (Rectangle) r;
-            rect.heightProperty().bind(cellSize.multiply(3));
+            if (rect.getId() != null && rect.getId().equals("bottomWhite")) {
+                rect.heightProperty().bind(cellSize.multiply(3).subtract(10));
+                rect.translateYProperty().bind(cellSize.add(5));
+            } else {
+                rect.heightProperty().bind(cellSize.multiply(3));
+                rect.translateYProperty().bind(cellSize);
+            }
+
             rect.widthProperty().bind(stack.widthProperty().subtract(stack.widthProperty().multiply(0.09)));
-            rect.translateYProperty().bind(cellSize);
         });
 
         List cc = stack.getChildren().stream().filter(child -> child.getClass().equals(VBox.class)).map(vbox -> {
@@ -67,9 +69,8 @@ public class Populator {
                 cellSize.bind(rectangle.heightProperty());
                 text.translateYProperty().bind(rectangle.heightProperty().subtract(fontSize).multiply(0.23));
                 fontSize.bind(rectangle.widthProperty().add(rectangle.heightProperty()).divide(4.5));
-
+                rectangle.setFill(Color.TRANSPARENT);
                 if (stack.getId().equals("paneSeconds")) {
-                    rectangle.setFill(Color.TRANSPARENT);
                     text.styleProperty().bind(Bindings.concat("-fx-font-size: ", fontSize.asString(), ";"
                             ,"-fx-base: rgb(100,100,100);", "-fx-effect: dropshadow( three-pass-box , rgba(255, 255, 255, 0.3), 1, 0.0, 1, 1 );"));
                 } else {
@@ -103,58 +104,4 @@ public class Populator {
 
         return new Column(cc, clocks, ColumnType.HOURS, clocksEvents[2]);
     }
-
-//    public Column create(Group group, ReadOnlyDoubleProperty widthProperty, ReadOnlyDoubleProperty heightProperty) {
-//
-//        final Random random = new Random();
-//        List<Cell> list =  IntStream.range(0, blockCount).mapToObj(i -> {
-//            Rectangle rectangle = new Rectangle(cellsize, 0, cellsize, cellsize);
-//            rectangle.setFill(Color.rgb(random.nextInt(255), random.nextInt(255), random.nextInt(255), 1));
-//            rectangle.setStrokeType(StrokeType.INSIDE);
-//            rectangle.setStroke(Color.BLACK);
-//            rectangle.setTranslateY(cellsize * i);
-//
-//            final Text label = new Text();
-//
-//            String id = "";
-//
-//            if (group.getId().equals("seconds")) {
-//                id = i + "seconds";
-//            }
-//
-//            if (group.getId().equals("minutes")) {
-//                id = i + "minutes";
-//            }
-//
-//            rectangle.xProperty().bind(widthProperty.divide(2).subtract(rectangle.widthProperty().divide(2)));
-//            rectangle.yProperty().bind(heightProperty.divide(2).subtract(rectangle.heightProperty().multiply(blockCount).divide(2)));
-//
-//            label.setFont(Font.font(20));
-//
-////            label.xProperty().bind(rectangle.widthProperty().divide(2).subtract(label.xProperty().divide(2)));
-//            label.xProperty().bind(rectangle.xProperty().add(rectangle.widthProperty().divide(2)));
-//            label.yProperty().bind(rectangle.translateYProperty().add(rectangle.heightProperty().divide(2)));
-//            label.setTextAlignment(TextAlignment.CENTER);
-//            label.setTextOrigin(VPos.CENTER);
-//
-//            label.setId(id);
-//            rectangle.setId(id);
-//
-////            group.getChildren().addAll(rectangle, label);
-//
-//            TranslateTransition translateTransition = new TranslateTransition();
-//            translateTransition.setNode(rectangle);
-//            return new Cell(rectangle, new Location(new Point2D(rectangle.getTranslateX(), rectangle.getTranslateY())), label, translateTransition, deltaStream);
-//        }).collect(Collectors.toList());
-//
-//        if (group.getId().equals("seconds")) {
-//            return new Column(list, clocks, ColumnType.SECONDS, clocksEvents[0]);
-//        }
-//
-//        if (group.getId().equals("minutes")) {
-//            return new Column(list, clocks, ColumnType.MINUTES, clocksEvents[1]);
-//        }
-//
-//        return new Column(list, clocks, ColumnType.HOURS, clocksEvents[2]);
-//    }
 }
