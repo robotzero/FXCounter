8172824f3a13355799d16a4f32aee0eeccb6bf68
Agnieszka K. Kocinski <0robotzero@gmail.com>
2018-06-22 21:21:26 +0100

    reset pt 2

--- src/main/java/com/robotzero/counter/service/ResetService.java
+++ src/main/java/com/robotzero/counter/service/ResetService.java
@@ -14,6 +14,7 @@ import io.reactivex.subjects.PublishSubject;
 
 import java.time.Duration;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -27,6 +28,12 @@ import static java.lang.Math.toIntExact;
 public class ResetService {
 
     private CurrentClockState currentClockState;
+    private final long baseTop = ChronoUnit.SECONDS.getDuration().toMinutes();
+    private final long baseMiddle = 30;
+    private final long baseBottom = 1;
+    private final long hoursTop = ChronoUnit.HOURS.getDuration().toHours();
+    private final long hoursMiddle = 12;
+    private final long hoursBottom = 1;
 
     public ResetService(PublishSubject<CurrentClockState> currentClockStateObservable) {
         currentClockStateObservable.subscribe(currentClockState -> {
@@ -39,10 +46,10 @@ public class ResetService {
         if (buttonType.equals(ButtonType.RIGHT)) {
             List<Action> actions = new ArrayList<>();
             actions.add(new ClickAction(ActionType.valueOf(buttonType.name()), buttonState));
-            List<Integer> tickNumber = howManyTicks();
-            int seconds = tickNumber.get(0);
-            int minutes = tickNumber.get(1);
-            int hours = tickNumber.get(2);
+            List<Long> tickNumber = howManyTicks();
+            int seconds = Math.toIntExact(tickNumber.get(0)) < 0 ? Math.abs(Math.toIntExact(tickNumber.get(0))) : Math.abs(Math.toIntExact(tickNumber.get(0)) * -1);
+            int minutes = Math.toIntExact(tickNumber.get(1));
+            int hours = Math.toIntExact(tickNumber.get(2));
             actions.addAll(IntStream.range(0, Math.abs(seconds)).mapToObj(index -> {
                 return new TickAction(seconds, ColumnType.SECONDS, TimerType.SCROLL);
             }).collect(Collectors.toList()));
@@ -53,7 +60,7 @@ public class ResetService {
                 return new TickAction(hours, ColumnType.HOURS, TimerType.SCROLL);
             }).collect(Collectors.toList()));
 
-            return Observable.zip(Observable.fromIterable(actions), Observable.interval(100, TimeUnit.MILLISECONDS), (left, right) -> left);
+            return Observable.zip(Observable.fromIterable(actions), Observable.interval(500, TimeUnit.MILLISECONDS), (iterable, interval) -> iterable);
         }
         return Observable.just(new ClickAction(
                         ActionType.valueOf(buttonType.name()),
@@ -62,16 +69,24 @@ public class ResetService {
         );
     }
 
-    private List<Integer> howManyTicks() {
-        CurrentClockState currentClockState1 = Optional.ofNullable(this.currentClockState).orElseGet(() -> new CurrentClockState(timeToReset().getSecond(), timeToReset().getMinute(), timeToReset().getHour(), null, false, false, false));
+    private List<Long> howManyTicks() {
         LocalTime toReset = timeToReset();
-        int elapsedSeconds = toIntExact(Duration.between(timeToReset(), LocalTime.of(toReset.getHour(), toReset.getMinute(), currentClockState1.getSecond())).toSeconds());
-        int elapsedMinutes = toIntExact(Duration.between(timeToReset(), LocalTime.of(toReset.getHour(), currentClockState1.getMinute(), toReset.getSecond())).toMinutes());
-        int elapsedHours = toIntExact(Duration.between(timeToReset(), LocalTime.of(currentClockState1.getHour(), toReset.getMinute(), toReset.getSecond())).toHours());
-        return Arrays.asList(elapsedSeconds, elapsedMinutes, elapsedHours);
+        CurrentClockState currentClockState = Optional.ofNullable(this.currentClockState).orElseGet(() -> new CurrentClockState(toReset.getSecond(), toReset.getMinute(), toReset.getHour(), null, false, false, false));
+        long seconds = ChronoUnit.SECONDS.between(toReset, toReset.withSecond(currentClockState.getSecond())) > baseMiddle ? baseBottom + (baseTop - ChronoUnit.SECONDS.between(toReset, toReset.withSecond(currentClockState.getSecond()))) : ChronoUnit.SECONDS.between(toReset, toReset.withSecond(currentClockState.getSecond()));
+        long minutes = ChronoUnit.MINUTES.between(toReset, toReset.withMinute(currentClockState.getMinute())) > baseMiddle ? baseBottom + (baseTop - ChronoUnit.MINUTES.between(toReset, toReset.withMinute(currentClockState.getMinute()))) : ChronoUnit.MINUTES.between(toReset, toReset.withMinute(currentClockState.getMinute()));
+        long hours = ChronoUnit.HOURS.between(toReset, toReset.withHour(currentClockState.getHour())) > hoursMiddle ? hoursBottom + (hoursTop - ChronoUnit.HOURS.between(toReset, toReset.withHour(currentClockState.getHour()))) : ChronoUnit.HOURS.between(toReset, toReset.withHour(currentClockState.getHour()));
+
+        System.out.println(seconds);
+        System.out.println(minutes);
+        System.out.println(hours);
+        int elapsedSeconds = toReset.getSecond() < currentClockState.getSecond() ? (toReset.getSecond() + 60 - currentClockState.getSecond()) : (toReset.getSecond() - currentClockState.getSecond()) * -1;
+        int elapsedMinutes = toReset.getMinute() < currentClockState.getMinute() ? toReset.getMinute() + 60 - currentClockState.getMinute() : (toReset.getMinute() - currentClockState.getMinute()) * -1;
+        int elapsedHours = toReset.getHour() < currentClockState.getHour() ? toReset.getHour() + 24 - currentClockState.getHour() : (toReset.getHour() - currentClockState.getHour()) * -1;
+
+        return Arrays.asList(seconds, minutes, hours);
     }
 
     private LocalTime timeToReset() {
-        return LocalTime.of(22, 01, 10);
+        return LocalTime.of(22, 1, 10);
     }
 }
